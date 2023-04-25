bbfda80808079f1eecea2832430fba3a61de66aa
Syrine Chennoufi <syrine.chennoufi@polytechnique.edu>
2023-04-25 10:09:04 +0200

    news

--- process_data.py
+++ process_data.py
@@ -2,19 +2,16 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn.preprocessing import normalize
-
-# Function to return the constant value columns of a given DataFrame
-def remove_constant_value_features(df):
-    return [e for e in df.columns if df[e].nunique() == 1]
+from sklearn.feature_selection import VarianceThreshold
 
 
 def process(csv_file):
+    sel=VarianceThreshold(0)
     my_data=pd.read_csv('all_data.csv')
     my_data.index=my_data['Patient_id']
     my_data=my_data.drop(['Patient_id'],axis=1)
-    drop_col = remove_constant_value_features(my_data)
-    my_data=my_data.drop(drop_col, axis=1)
     my_data=my_data.dropna() 
+    my_data=pd.DataFrame(data=sel.fit_transform(my_data),index=my_data.index, columns=sel.get_feature_names_out(None))
     return my_data
 
 
@@ -30,5 +27,6 @@ def normalize_data(my_data,ax):
     return res
 
 
-
+def log2_transform (X):
+    return np.log2(X)
 
